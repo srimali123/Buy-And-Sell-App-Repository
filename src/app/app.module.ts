d653f0b9a568100626da695c786aea29562b32b6
srimali <srimaliudayangani850@gmail.com>
2021-06-05 02:06:06 +0530

    First Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingsPageComponent,
+    HomePageComponent,
+    ListingDetailPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
